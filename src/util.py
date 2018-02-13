1bc4626ef69a97edf39b9082c3c28b3d067adf64
Christophe Simonis <chs@odoo.com>
2018-02-13 11:58:24 +0100

    [FIX] util.indirect_references(): typo in table name

--- src/util.py
+++ src/util.py
@@ -1313,7 +1313,7 @@ def indirect_references(cr, bound_only=False):
         IR('mail_activity', 'res_model', 'res_id', 'res_model_id'),
         IR('mail_alias', None, 'alias_force_thread_id', 'alias_model_id'),
         IR('mail_alias', None, 'alias_parent_thread_id', 'alias_parent_model_id'),
-        IR('mail-followers', 'res_model', 'res_id'),
+        IR('mail_followers', 'res_model', 'res_id'),
         IR('mail_message_subtype', 'res_model', None),
         IR('mail_message', 'model', 'res_id'),
         IR('mail_compose_message', 'model', 'res_id'),
