1647aabbbfc6721349260e3f4bea6e3c48376cbe
Christophe Simonis <chs@odoo.com>
2019-01-21 17:35:13 +0100

    [FIX] util,base@*: make merge of modules always tolerant

--- src/util.py
+++ src/util.py
@@ -865,13 +865,13 @@ def rename_module(cr, old, new):
                      AND model = %s
                """, (mod_new, mod_old, 'base', 'ir.module.module'))
 
-def merge_module(cr, old, into, tolerant=False):
+def merge_module(cr, old, into):
     """Move all references of module `old` into module `into`
     """
     cr.execute("SELECT name, id FROM ir_module_module WHERE name IN %s", [(old, into)])
     mod_ids = dict(cr.fetchall())
 
-    if tolerant and old not in mod_ids:
+    if old not in mod_ids:
         # this can happen in case of temp modules added after a release if the database does not
         # know about this module, i.e: account_full_reconcile in 9.0
         # `into` should be know. Let it crash if not
