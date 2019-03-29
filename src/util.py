0dbdb665852ae219c97b320a17713a8b4e6dfaf1
Damien Bouvy <dbo@odoo.com>
2019-03-29 17:27:54 +0100

    [ADD] util.fixup_m2m
    Ensure m2m table have correct FKs and that there aren't any duplicated entries.

--- src/util.py
+++ src/util.py
@@ -664,6 +664,92 @@ def create_m2m(cr, m2m, fk1, fk2, col1=None, col2=None):
         CREATE INDEX ON {m2m}({col2});
     """.format(**locals()))
 
+def fixup_m2m(cr, m2m, fk1, fk2, col1=None, col2=None):
+    if col1 is None:
+        col1 = '%s_id' % fk1
+    if col2 is None:
+        col2 = '%s_id' % fk2
+
+    if not table_exists(cr, m2m):
+        return
+
+    def target_of(cr, table, column):
+        cr.execute("""
+            SELECT con.conname, cl2.relname, att2.attname
+            FROM pg_constraint con
+            JOIN pg_class cl1 ON (con.conrelid = cl1.oid)
+            JOIN pg_attribute att1 ON (    array_lower(con.conkey, 1) = 1
+                                        AND con.conkey[1] = att1.attnum
+                                        AND att1.attrelid = cl1.oid)
+            JOIN pg_class cl2 ON (con.confrelid = cl2.oid)
+            JOIN pg_attribute att2 ON (    array_lower(con.confkey, 1) = 1
+                                        AND con.confkey[1] = att2.attnum
+                                        AND att2.attrelid = cl2.oid)
+            WHERE cl1.relname = %s
+            AND att1.attname = %s
+            AND con.contype = 'f'
+        """, [table, column])
+        return cr.fetchone()
+
+    # cleanup
+    cr.execute("""
+        DELETE FROM {m2m} t
+              WHERE {col1} IS NULL
+                 OR {col2} IS NULL
+                 OR NOT EXISTS (SELECT id FROM {fk1} WHERE id=t.{col1})
+                 OR NOT EXISTS (SELECT id FROM {fk2} WHERE id=t.{col2})
+    """.format(**locals()))
+    deleted = cr.rowcount
+    if deleted:
+        _logger.debug("%(m2m)s: removed %(deleted)d invalid rows", locals())
+
+    # remove duplicated rows
+    cr.execute("""
+        DELETE FROM {m2m}
+              WHERE ctid IN (SELECT ctid
+                               FROM (SELECT ctid,
+                                            ROW_NUMBER() OVER (PARTITION BY {col1}, {col2}
+                                                                   ORDER BY ctid) as rnum
+                                       FROM {m2m}) t
+                              WHERE t.rnum > 1)
+    """.format(**locals()))
+    deleted = cr.rowcount
+    if deleted:
+        _logger.debug("%(m2m)s: removed %(deleted)d duplicated rows", locals())
+
+    # set not null
+    cr.execute("ALTER TABLE {m2m} ALTER COLUMN {col1} SET NOT NULL".format(**locals()))
+    cr.execute("ALTER TABLE {m2m} ALTER COLUMN {col2} SET NOT NULL".format(**locals()))
+
+    # create  missing or bad fk
+    target = target_of(cr, m2m, col1)
+    if target and target[1:] != [fk1, 'id']:
+        cr.execute("ALTER TABLE {m2m} DROP CONSTRAINT {con}".format(m2m=m2m, con=target[0]))
+        target = None
+    if not target:
+        _logger.debug("%(m2m)s: add FK %(col1)s -> %(fk1)s", locals())
+        cr.execute("ALTER TABLE {m2m} ADD FOREIGN KEY ({col1}) REFERENCES {fk1} ON DELETE CASCADE"
+                   .format(**locals()))
+
+    target = target_of(cr, m2m, col2)
+    if target and target[1:] != [fk2, 'id']:
+        cr.execute("ALTER TABLE {m2m} DROP CONSTRAINT {con}".format(m2m=m2m, con=target[0]))
+        target = None
+    if not target:
+        _logger.debug("%(m2m)s: add FK %(col2)s -> %(fk2)s", locals())
+        cr.execute("ALTER TABLE {m2m} ADD FOREIGN KEY ({col2}) REFERENCES {fk2} ON DELETE CASCADE"
+                   .format(**locals()))
+
+    # create indexes
+    idx = get_index_on(cr, m2m, col1, col2)
+    if not idx or not idx[1]:
+        cr.execute("CREATE UNIQUE INDEX ON %s (%s, %s)" % (m2m, col1, col2))
+
+    if not get_index_on(cr, m2m, col1):
+        cr.execute('CREATE INDEX ON "%s" ("%s")' % (m2m, col1))
+    if not get_index_on(cr, m2m, col2):
+        cr.execute('CREATE INDEX ON "%s" ("%s")' % (m2m, col2))
+
 def uniq_tags(cr, model, uniq_column='name', order='id'):
     """
         Deduplicated "tag" models entries.
