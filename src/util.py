a0780da9399c32aa5334f1f5edd723125c9259dd
Christophe Simonis <chs@odoo.com>
2019-05-02 12:03:07 +0200

    [DOC] util.dispatch_by_dbuuid

--- src/util.py
+++ src/util.py
@@ -206,6 +206,17 @@ def dbuuid(cr):
     return cr.fetchone()[0]
 
 def dispatch_by_dbuuid(cr, version, callbacks):
+    """
+    Allow to execute a migration script for a specific database only, base on its dbuuid.
+    Example:
+
+    >>> def db_yellowbird(cr, version):
+            cr.execute("DELETE FROM ir_ui_view WHERE id=837")
+
+    >>> dispatch_by_dbuuid(cr, version, {
+            'ef81c07aa90936a89f4e7878e2ebc634a24fcd66': db_yellowbird,
+        })
+    """
     uuid = dbuuid(cr)
     if uuid in callbacks:
         func = callbacks[uuid]
