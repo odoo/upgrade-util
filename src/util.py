0f444b862041c2c23dce1ece7f99c432a291a829
Christophe Simonis <chs@odoo.com>
2019-08-22 17:00:34 +0200

    [FIX] util.convert_binary_field_to_attachment: ignore more invalid data

--- src/util.py
+++ src/util.py
@@ -1655,7 +1655,7 @@ def convert_binary_field_to_attachment(cr, model, field, encoded=True):
         # we can't save create the attachment with res_model & res_id as it will fail computing
         # `res_name` field for non-loaded models. Store it naked and change it via SQL after.
         data = bytes(data)
-        if re.match(br"^\d+ bytes$", data):
+        if re.match(br"^\d+ (bytes|[KMG]b)$", data, re.I):
             # badly saved data, no need to create an attachment.
             continue
         if not encoded:
