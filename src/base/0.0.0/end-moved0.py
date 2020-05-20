2357016508795e38e0cb21ce37d2db66e0d5313f
Rémy Baranx (bar) <bar@odoo.com>
2020-05-20 05:30:53 +0000

    [IMP] base@0.0.0: drop moved0 field if the column is empty
    At the end of the upgrade, if some `moved0` fields are still
    present but all the column is empty, we can just drop them
    to avoid raising a useless migration exception.
    
    Upgrade requests: 47707, 47142, 46506, 45713, 45580, 43998
    
    Part of odoo/upgrade#1236
    
    Signed-off-by: Christophe Simonis <chs@odoo.com>

--- src/base/0.0.0/end-moved0.py
+++ src/base/0.0.0/end-moved0.py
@@ -11,10 +11,21 @@ def migrate(cr, version):
     """)  # noqa:Q440
     # fmt:on
 
-    new_moved0 = set(tuple(moved) for moved in cr.fetchall()) - util.ENVIRON["moved0"]
+    moved_fields = set(tuple(moved) for moved in cr.fetchall()) - util.ENVIRON["moved0"]
 
-    if new_moved0:
+    remaining_fields = []
+    for f in moved_fields:
+        # if the moved column is empty, just remove it
+        cr.execute("SELECT EXISTS(SELECT 1 FROM {0} WHERE {1} IS NOT NULL)".format(*f))
+
+        if not cr.fetchone()[0]:
+            cr.execute("ALTER TABLE {0} DROP COLUMN {1}".format(*f))
+            continue
+
+        remaining_fields.append(f)
+
+    if remaining_fields:
         raise util.MigrationError(
             "New `moved0` field. It happen when the ORM cannot change a column type by itself.\n%s"
-            % "\n".join("\t- %s.%s" % m for m in sorted(new_moved0))
+            % "\n".join("\t- %s.%s" % m for m in sorted(remaining_fields))
         )
