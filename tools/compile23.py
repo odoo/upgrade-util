9fa87d7a3b55a021febc58e481b6a71f01eddd1a
Christophe Simonis <chs@odoo.com>
2021-04-08 12:16:17 +0000

    [FIX] tools/compile23.py: compile all python files
    The patterns now only match the files that should be compiled in one
    python version. All non matched python files are compiles with both
    versions.

--- tools/compile23.py
+++ tools/compile23.py
@@ -3,17 +3,15 @@ import subprocess
 import sys
 from pathlib import PurePath
 
-py2_patterns = [
-    "src/*.py",
-    "src/*/0.0.0/*.py",
+py2_only_patterns = [
 ]
 py2_files = []
 
-py3_patterns = [
+py3_only_patterns = [
     "tools/*.py",
-    "src/*.py",
+    # tests are only run from version 12. python2 compatibility is not needed.
+    "src/testing.py",
     "src/*/tests/*.py",
-    "src/*/0.0.0/*.py",
 ]
 py3_files = []
 
@@ -25,12 +23,16 @@ for filename in sys.argv[1:]:
         continue
 
     if not filename.islower():
-        print(f"filename {filename!r} is not lowecase")
+        print(f"filename {filename!r} is not lowercase")
         rc = 1
 
-    if any(p.match(pattern) for pattern in py2_patterns):
+    if any(p.match(pattern) for pattern in py2_only_patterns):
         py2_files.append(filename)
-    if any(p.match(pattern) for pattern in py3_patterns):
+    elif any(p.match(pattern) for pattern in py3_only_patterns):
+        py3_files.append(filename)
+    else:
+        # not an explicit match to a python version. Test against both versions.
+        py2_files.append(filename)
         py3_files.append(filename)
 
 
