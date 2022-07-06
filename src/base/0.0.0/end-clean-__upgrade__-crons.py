ebbfe17995af7daf9fa443bf7f5967066b1543cc
erl-odoo <erl@odoo.com>
2022-07-06 16:45:06 +0000

    [FIX] base/0.0.0: delete cron related server actions too
    The cron cleaning script is modified to remove both crons and the
    related server actions too.
    
    Part-of: odoo/upgrade#3580

--- src/base/0.0.0/end-clean-__upgrade__-crons.py
+++ src/base/0.0.0/end-clean-__upgrade__-crons.py
@@ -3,18 +3,24 @@ from odoo.addons.base.maintenance.migrations import util
 
 
 def migrate(cr, version):
+    server_act_array = (
+        "array_agg(c.ir_actions_server_id)" if util.column_exists(cr, "ir_cron", "ir_actions_server_id") else "NULL"
+    )
     xid_date = (
         ", x.date_init" if util.column_exists(cr, "ir_model_data", "date_init") else ""
     )  # field gone in saas-13.4
     cr.execute(
         """
-            SELECT array_agg(c.id)
+            SELECT array_agg(c.id), {server_act_array}
               FROM ir_cron c
               JOIN ir_model_data x ON x.model = 'ir.cron' AND x.res_id = c.id
              WHERE x.module = '__upgrade__'
                AND now() - COALESCE(c.create_date, x.create_date {xid_date}) > interval '1 month'
         """.format(
-            xid_date=xid_date
+            server_act_array=server_act_array,
+            xid_date=xid_date,
         )
     )
-    util.remove_records(cr, "ir.cron", cr.fetchone()[0])
+    cron_ids, server_act_ids = cr.fetchone()
+    util.remove_records(cr, "ir.cron", cron_ids)
+    util.remove_records(cr, "ir.actions.server", server_act_ids)
