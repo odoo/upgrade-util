3584efca60ee4731b8b81eff3d1dc3c318779e98
arg-odoo <arg@odoo.com>
2023-01-20 12:53:18 +0000

    [IMP] util/inconsistencies.py: Add `verify_{uoms,products}` functions
    UoM issues in upgrade are not always quite clear: the same error can
    happen in multiple places and then is not everytime easy to understand.
    The issue is the following:
    
    ```
    The unit of measure %s defined on the order line doesn't belong to the
    same category as the unit of measure %s defined on the product. Please
    correct the unit of measure defined on the order line or on the
    product, they should belong to the same category.
    ```
    
    To improve the standard uom issue message, two new functions has been
    created:
    - `verify_uoms`: This method will ensure that every uom
    defined on table lines match the same category than which is defined
    on the product template. If some inconsistencies are found, an error
    will be raised to list every line that have mismatching uom category.
    - `verify_products`: This method will ensure that every
    product defined on table lines match the product defined on the
    foreign_table. If some inconsistencies are found, an error will be
    raised to list every line that have mismatching product.
    
    upg-470381
    upg-462352
    upg-437441
    upg-418532
    (Lots of request have failed on it, about 500+ taking achived ones into
    account.)
    
    tbg-71
    
    Part-of: odoo/upgrade#4066

--- src/util/inconsistencies.py
+++ src/util/inconsistencies.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 import logging
+from textwrap import dedent
 
+from psycopg2.extensions import quote_ident
+
+from .exceptions import MigrationError
 from .helpers import _validate_model, table_of_model
+from .misc import chunks
 from .report import add_to_migration_reports
 
 _logger = logging.getLogger(__name__)
@@ -96,3 +101,147 @@ def verify_companies(
             category="Multi-company inconsistencies",
             format="html",
         )
+
+
+def verify_uoms(cr, model, uom_field="product_uom_id", product_field="product_id", ids=None):
+    """
+    Check if the category of uom  on `model` is the same as the category of uom on `product.template`.
+    When `ids` is not provided, every ids would be verified.
+    """
+    _validate_model(model)
+    table = table_of_model(cr, model)
+
+    q = lambda s: quote_ident(s, cr._cnx)
+    query = """
+        SELECT t.id,
+               t.{uom_column},
+               tu.category_id,
+               pt.uom_id,
+               ptu.category_id
+        FROM {table} t
+        JOIN uom_uom tu ON t.{uom_column} = tu.id
+        JOIN product_product pp ON t.{product_column} = pp.id
+        JOIN product_template pt ON pp.product_tmpl_id = pt.id
+        JOIN uom_uom ptu ON pt.uom_id = ptu.id
+        WHERE tu.category_id != ptu.category_id
+    """.format(
+        table=q(table),
+        uom_column=q(uom_field),
+        product_column=q(product_field),
+    )
+
+    if ids is None:
+        cr.execute(query)
+        rows = cr.fetchall()
+    elif ids:
+        query += " AND t.id IN %s"
+        rows = []
+        ids_chunks = chunks(ids, size=cr.IN_MAX, fmt=tuple)
+        for chunk in ids_chunks:
+            cr.execute(query, [chunk])
+            rows.extend(cr.fetchall())
+    else:
+        return
+
+    if not rows:
+        return
+
+    title = model.replace(".", " ").title()
+    msg = dedent(
+        """
+        There is a UoM mismatch in some {title}s. The category of the UoM defined on the
+        {title} is different from that defined on the Product Template. To allow the upgrade to
+        continue, the UoM categories on the {title} and on the Product Template must be the same.
+        These {title}s have inconsistencies:
+        """.format(
+            **locals()
+        )
+    )
+    msg += "\n".join(
+        "  * {}(id={}) has UoM(id={},category={}), Product Template has UoM(id={},category={})".format(
+            title, line_id, line_uom, line_uom_category, product_uom, product_uom_category
+        )
+        for line_id, line_uom, line_uom_category, product_uom, product_uom_category in rows
+    )
+    raise MigrationError("\n{}\n".format(msg))
+
+
+def verify_products(
+    cr, model, foreign_model, foreign_model_reference_field, model_product_field="product_id", ids=None
+):
+    """
+    Check if the product on the `foreign_model` is the same as the product on the `model`.
+    When `ids` is not provided, every ids would be verified.
+
+    The `foreign_model` should be the one that have a reference to the `model` using this
+    schema:
+        >>> `foreign_model`.`foreign_reference_field` = `model`.id
+
+    In case of the `model` own a product field that is not `product_id`, you can specify the
+    `model_product_field` argument following this schema:
+        >>> `model`.`model_product_field` = product_product.id
+
+    For example, if you want to check if the product defined on the `account_move_line`
+    is the same as the product defined on the `purchase_order_line` using `purchase_line_id`
+    as reference, you should call this function in this way:
+        >>> verify_products(cr, "purchase.order.line", "account.move.line", "purchase_line_id", ids=ids)
+
+    """
+    _validate_model(model)
+    _validate_model(foreign_model)
+    table = table_of_model(cr, model)
+    foreign_table = table_of_model(cr, foreign_model)
+
+    q = lambda s: quote_ident(s, cr._cnx)
+    query = """
+        SELECT f.id,
+               f.product_id,
+               t.id,
+               t.product_id
+          FROM {table} t
+          JOIN {foreign_table} f ON f.{foreign_model_reference_field} = t.id
+          JOIN product_product pp ON t.{model_product_field} = pp.id
+          JOIN product_template pt ON pp.product_tmpl_id = pt.id
+         WHERE f.product_id != t.product_id
+    """.format(
+        table=q(table),
+        foreign_table=q(foreign_table),
+        foreign_model_reference_field=q(foreign_model_reference_field),
+        model_product_field=q(model_product_field),
+    )
+
+    if ids is None:
+        cr.execute(query)
+        rows = cr.fetchall()
+    elif ids:
+        query += " AND t.id IN %s"
+        rows = []
+        ids_chunks = chunks(ids, size=cr.IN_MAX, fmt=tuple)
+        for chunk in ids_chunks:
+            cr.execute(query, [chunk])
+            rows.extend(cr.fetchall())
+    else:
+        return
+
+    if not rows:
+        return
+
+    title = model.replace(".", " ").title()
+    foreign_title = foreign_model.replace(".", " ").title()
+    msg = dedent(
+        """
+        There is a product mismatch in some {foreign_title}. The product defined on the {foreign_title}
+        is different from that defined on the {title}. To allow the upgrade to continue, the product
+        on the {foreign_title} and on the {title} must be the same.
+        These {foreign_title} have inconsistencies:
+        """.format(
+            **locals()
+        )
+    )
+    msg += "\n".join(
+        "  * {}(id={}) has Product(id={}), {}(id={}) has Product(id={})".format(
+            foreign_title, foreign_line_id, foreign_line_product, title, line_id, line_product
+        )
+        for foreign_line_id, foreign_line_product, line_id, line_product in rows
+    )
+    raise MigrationError("\n{}\n".format(msg))
