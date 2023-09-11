b520cbfe874102727c8818db0260f0fc4112c343
Gauthier Wala (gawa) <gawa@odoo.com>
2023-09-11 11:02:45 +0200

    [FIX] util.helpers: Add exceptions for l10n_pl_tax_office
    Added new modules but with wrong name for a new model.
    As it can't be changed without script, add it to the exceptions.
    
    Part of odoo/upgrade#5127
    
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- src/util/helpers.py
+++ src/util/helpers.py
@@ -5,7 +5,7 @@ import os
 import lxml
 
 from .exceptions import SleepyDeveloperError
-from .misc import splitlines, version_gte
+from .misc import splitlines, version_between, version_gte
 
 _logger = logging.getLogger(__name__.rpartition(".")[0])
 
@@ -20,6 +20,7 @@ _VALID_MODELS = frozenset(
         "o2m_changes_parent",
         "o2m_changes_children",
     }
+    | ({"l10n_pl_tax_office"} if version_between("16.0", "17.0") else set())
     | {m.strip() for m in os.getenv("UPG_VALID_MODELS", "").split(";")} - {""}
 )
 
