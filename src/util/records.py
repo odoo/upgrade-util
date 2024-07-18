249d2cbb91c7536199f67076e8feab8e1dc167c7
kmod-odoo <kmod@odoo.com>
2024-07-18 10:31:32 +0000

    [IMP] util.remove_view : remove redundant t-calls
    while removing the view, removing the content having t-call in other views which 
    are calling to have the content of it.
    As there are many specific fixes available for this, it's better to handle 
    it in remove_view.
    
    Before fix:
    t-call with the same xml_id will remain in other views that are using it.
    So during access of that view, the system is raising an error "view not found."
    ```
    <t name="Payment" t-name="website_sale.payment">
        <t t-call="website_sale.cart_summary"/>
    </t>
    ```
    
    After fix:
    t-call will be removed, so no error will be raised.
    ```
    <t name="Payment" t-name="website_sale.payment">
    </t>
    ```
    
    Traceback:
    ```
    Error while render the template
    ValueError: View 'website_sale.cart_summary' in website 1 not found
    Template: website_sale.payment
    Path: /t/t/div/div[1]/div/div[4]/div[1]/t
    Node: <t t-call="website_sale.address_on_payment"/>
    ```

--- src/util/records.py
+++ src/util/records.py
@@ -12,12 +12,12 @@ from psycopg2 import sql
 from psycopg2.extras import Json, execute_values
 
 try:
-    from odoo import release
+    from odoo import modules, release
     from odoo.tools.convert import xml_import
     from odoo.tools.misc import file_open
     from odoo.tools.translate import xml_translate
 except ImportError:
-    from openerp import release
+    from openerp import modules, release
     from openerp.tools.convert import xml_import
     from openerp.tools.misc import file_open
 
@@ -105,6 +105,16 @@ def remove_view(cr, xml_id=None, view_id=None, silent=False, key=None):
         cr.execute("SELECT id FROM ir_ui_view WHERE key = %s AND id != %s", [xml_id, view_id])
         for [v_id] in cr.fetchall():
             remove_view(cr, view_id=v_id, silent=silent, key=xml_id)
+
+    if not key and column_exists(cr, "ir_ui_view", "key"):
+        cr.execute("SELECT key FROM ir_ui_view WHERE id = %s and key != %s", [view_id, xml_id])
+        [key] = cr.fetchone() or [None]
+
+    # Occurrences of xml_id and key in the t-call of views are to be found and removed.
+    if xml_id != "?":
+        _remove_redundant_tcalls(cr, xml_id)
+    if key and key != xml_id:
+        _remove_redundant_tcalls(cr, key)
 
     if not view_id:
         return
@@ -1784,3 +1794,47 @@ def remove_act_window_view_mode(cr, model, view_mode):
         """,
         [view_mode, default, model, view_mode, view_mode],
     )
+
+
+def _remove_redundant_tcalls(cr, match):
+    """
+    Remove t-calls of the removed view.
+
+    This function removes the t-calls to `match`.
+
+    :param str match: t-calls value to remove, typically it would be a view's xml_id or key
+    """
+    arch_col = (
+        get_value_or_en_translation(cr, "ir_ui_view", "arch_db")
+        if column_exists(cr, "ir_ui_view", "arch_db")
+        else "arch"
+    )
+    cr.execute(
+        format_query(
+            cr,
+            """
+            SELECT iv.id,
+                   imd.module,
+                   imd.name
+              FROM ir_ui_view iv
+         LEFT JOIN ir_model_data imd
+                ON iv.id = imd.res_id
+               AND imd.model = 'ir.ui.view'
+             WHERE {} ~ %s
+        """,
+            sql.SQL(arch_col),
+        ),
+        [r"""\yt-call=(["']){}\1""".format(re.escape(match))],
+    )
+    standard_modules = set(modules.get_modules()) - {"studio_customization"}
+    for vid, module, name in cr.fetchall():
+        with edit_view(cr, view_id=vid) as arch:
+            for node in arch.findall(".//t[@t-call='{}']".format(match)):
+                node.getparent().remove(node)
+        if not module or module not in standard_modules:
+            _logger.info(
+                "The view %swith ID: %s has been updated, removed t-calls to deprecated %r",
+                ("`{}.{}` ".format(module, name) if module else ""),
+                vid,
+                match,
+            )
