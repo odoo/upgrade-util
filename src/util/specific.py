9ff74a9dd799b0ca76d8b659d138a8ad1364336a
Edoardo Piroli <pied@odoo.com>
2024-10-31 14:47:21 +0100

    [IMP] util.dispatch_by_dbuuid
    See https://github.com/odoo/upgrade-platform/pull/705
    
    Make dispatch_by_dbuuid able to dispatch callbacks to a db without affecting its
    relatives[^1].
    
    [^1]: ancestors, siblings and descendants.

--- src/util/specific.py
+++ src/util/specific.py
@@ -11,39 +11,58 @@ from .report import add_to_migration_reports
 _logger = logging.getLogger(__name__)
 
 
-@_cached
 def dbuuid(cr):
+    return _dbuuids(cr)[-1]
+
+
+@_cached
+def _dbuuids(cr):
     cr.execute(
         """
-        SELECT value
+        SELECT REPLACE(value, 'upg-neuter-', '')
           FROM ir_config_parameter
          WHERE key IN ('database.uuid', 'origin.database.uuid')
-      ORDER BY key DESC
-         LIMIT 1
-    """
+      ORDER BY key
+        """
     )
-    return cr.fetchone()[0]
+    return [uuid for (uuid,) in cr.fetchall()]
 
 
 def dispatch_by_dbuuid(cr, version, callbacks):
     """
-    Allow to execute a migration script for a specific database only, base on its dbuuid.
+    Allow to execute a migration script for a specific database only, based on its dbuuid.
 
-    Example:
-    -------
-    >>> def db_yellowbird(cr, version):
-            cr.execute("DELETE FROM ir_ui_view WHERE id=837")
+    .. example::
+        .. code-block:: python
+            def db_yellowbird(cr, version):
+                cr.execute("DELETE FROM ir_ui_view WHERE id=837")
 
-    >>> dispatch_by_dbuuid(cr, version, {
-            'ef81c07aa90936a89f4e7878e2ebc634a24fcd66': db_yellowbird,
-        })
+            util.dispatch_by_dbuuid(cr, version, {
+                "ef81c07aa90936a89f4e7878e2ebc634a24fcd66": db_yellowbird,
+            })
 
+    :param str version: Odoo version
+    :param dict[str, function] callbacks: mapping dbuuids to the functions to run against matching dbs
+
+    .. warning::
+        - Only the first match of (icp["database.uuid"], icp["origin.database.uuid"]) in `callbacks` is executed.
+
+    .. tip::
+        - If looking to prevent a callback from running against a descendant db, one can use a noop `callback`:
+        .. example::
+            .. code-block:: python
+                noop = lambda *args: None
+                util.dispatch_by_dbuuid(cr, version, {
+                    "dbuuid": noop,
+                    "ancestor's dbuuid": db_yellowbird,
+                })
     """
-    uuid = dbuuid(cr)
-    if uuid in callbacks:
-        func = callbacks[uuid]
-        _logger.info("calling dbuuid-specific function `%s`", func.__name__)
-        func(cr, version)
+    for uuid in _dbuuids(cr):
+        if uuid in callbacks:
+            func = callbacks[uuid]
+            _logger.info("calling dbuuid-specific function `%s`", func.__name__)
+            func(cr, version)
+            break
 
 
 def rename_custom_model(cr, model_name, new_model_name, custom_module=None, report_details=""):
