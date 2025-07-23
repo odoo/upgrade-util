b442be110fab2d413aa4a7ed45250d0a90e8ebd6
Fernando Figuereo <fernandorfr02@gmail.com>
2025-07-23 13:22:59 -0400

    [IMP] end-views-activation: Add missing view to the deletion list for improved migration accuracy

--- src/l10n_do_banks/17.0.1.0.0/end-views-activation.py
+++ src/l10n_do_banks/17.0.1.0.0/end-views-activation.py
@@ -159,8 +159,9 @@ def migrate(cr, version):
     delete_views_list = [
         'warranty_registration_extra_features.warranty_case_claims_form_view1_inherit',
         'studio_customization.odoo_studio_warranty_5e101e7c-e388-4589-b9b8-628b03ca43f1'
+        'studio_customization.odoo_studio_warranty_64540003-0f78-4e15-8bba-6d3475418fed'
     ]
-    
+
     activate_database_views(cr, activate_views_list)
     delete_database_views(cr, delete_views_list)
     remove_inherited_views(cr, remove_inherit_views_list)
