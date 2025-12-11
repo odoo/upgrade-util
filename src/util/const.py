a81a7efe9ae0d37ce300751e61be3827c713c0ac
Jinane Maksoud <maji@odoo.com>
2025-12-11 14:12:14 +0100

    [ADD] modules: check module force install
    Add a utility function to check if a given module
    was force installed during the upgrade.
    This can be useful when a module is force installed
    and another module upgrade scripts need to fill in
    some data for it.

--- src/util/const.py
+++ src/util/const.py
@@ -16,6 +16,7 @@ ENVIRON = {
     "__modules_to_skip_autoinstall": set(),
     "__fix_fk_allowed_cascade": [],
     "__no_model_data_delete": {},
+    "__force_installed_modules": set(),
 }
 
 NEARLYWARN = 25  # between info and warning; appear on runbot build page
