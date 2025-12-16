8ad9becac96496be5f65ba1a030534650de35e44
Jinane Maksoud <maji@odoo.com>
2025-12-16 10:21:47 +0100

    [FIX] snippet: Fix executor DeprecationWarning
    Python3.12 raises a DeprecationWarning when a multithreaded
    pool executor uses 'fork' as a starting method and it is
    recommended we use 'forkserver' instead. The 'forkserver'
    method is safer than 'fork' and faster than 'spawn', and it
    is the default in python3.14.
    https://docs.python.org/3/library/multiprocessing.html#multiprocessing-start-methods

--- src/util/snippets.py
+++ src/util/snippets.py
@@ -281,7 +281,11 @@ def convert_html_columns(cr, table, columns, converter_callback, where_column="I
     update_sql = ", ".join(f'"{column}" = %({column})s' for column in columns)
     update_query = f"UPDATE {table} SET {update_sql} WHERE id = %(id)s"
 
-    extrakwargs = {"mp_context": multiprocessing.get_context("fork")} if sys.version_info >= (3, 7) else {}
+    extrakwargs = (
+        {"mp_context": multiprocessing.get_context("forkserver" if sys.version_info >= (3, 12) else "fork")}
+        if sys.version_info >= (3, 7)
+        else {}
+    )
     with ProcessPoolExecutor(max_workers=get_max_workers(), **extrakwargs) as executor:
         convert = Convertor(converters, converter_callback)
         for query in log_progress(split_queries, logger=_logger, qualifier=f"{table} updates"):
