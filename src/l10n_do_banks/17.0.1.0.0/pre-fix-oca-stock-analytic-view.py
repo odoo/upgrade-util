b91cccbd51734acba8aaec6e0ff70a3eec6550a9
DanielAPereyraB <pereyradaniel2020@gmail.com>
2026-01-19 13:31:59 -0400

    fix(l10n_do_banks): update SQL query to replace 'analytic_account_id' with 'analytic_distribution' and include 'analytic_tag_ids' in stock.picking views

--- src/l10n_do_banks/17.0.1.0.0/pre-fix-oca-stock-analytic-view.py
+++ src/l10n_do_banks/17.0.1.0.0/pre-fix-oca-stock-analytic-view.py
@@ -10,12 +10,19 @@ def _update_views(cr):
         """
         UPDATE ir_ui_view
            SET arch_db = replace(
-               arch_db::text,
-               'analytic_account_id',
+               replace(
+                   arch_db::text,
+                   'analytic_account_id',
+                   'analytic_distribution'
+               ),
+               'analytic_tag_ids',
                'analytic_distribution'
            )::jsonb
          WHERE model = 'stock.picking'
-           AND arch_db::text ILIKE '%%analytic_account_id%%'
+           AND (
+               arch_db::text ILIKE '%%analytic_account_id%%'
+               OR arch_db::text ILIKE '%%analytic_tag_ids%%'
+           )
         """
     )
     _logger.info("Updated %s stock.picking views.", cr.rowcount)
