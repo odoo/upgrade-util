31639d1eeb567841f337817bf518c750769ae08f
Christophe Simonis <chs@odoo.com>
2026-03-30 14:10:57 +0200

    [ADD] @util.profiler.trace decorator

--- src/util/profiler.py
+++ src/util/profiler.py
@@ -1,10 +1,14 @@
+import inspect
 import logging
 import os
+import sys
+import time
+from collections import defaultdict
 from functools import partial, wraps
 from inspect import getsourcefile
 from pathlib import Path
 
-from .const import ENVIRON
+from .const import ENVIRON, NEARLYWARN
 from .misc import version_gte
 
 try:
@@ -13,7 +17,7 @@ except ImportError:
     if version_gte("15.0"):
         raise
 
-__all__ = ["profile"]
+__all__ = ["profile", "trace"]
 
 _logger = logging.getLogger(__name__)
 
@@ -111,3 +115,94 @@ else:
             return partial(profile, description=description, speedscope_file=speedscope_file)
         _logger.error("profiling is only available from Odoo 15")
         return func
+
+
+if sys.version_info >= (3, 7):
+    tick = time.perf_counter_ns
+    ms = lambda t: t // 1000000
+else:
+    tick = time.perf_counter if sys.version_info >= (3, 3) else time.time
+    ms = lambda t: int(t * 1000)
+
+if not os.isatty(logging.getLogger().handlers[0].stream.fileno()):
+
+    def _colorize(t):
+        return "%d" % (ms(t),)
+else:
+
+    def _colorize(t):
+        t = ms(t)
+
+        colors = [
+            (30000, 9),  # high intensity red
+            (10000, 1),  # red
+            (5000, 5),  # magenta
+            (1000, 3),  # yellow
+        ]
+        for limit, color in colors:
+            if t >= limit:
+                return "\033[38;5;%dm%s\033[0m" % (color, t)
+        return str(t)
+
+
+class _Tracer(object):
+    # code adapted from the Odoo 14 profiler.
+    def __init__(self):
+        self._ff = None
+        self._events = []
+        self._lines = None
+        self._firstline = 0
+        self._calls = []
+
+    def trace(self, frame, event, arg):
+        if self._ff is None:
+            self._ff = frame
+            self._lines, self._firstline = inspect.getsourcelines(frame)
+        if self._ff is not frame or frame.f_code.co_name == "<genexpr>":
+            return None
+        self._calls.append(
+            {
+                "l": frame.f_lineno,
+                "t": tick(),
+            }
+        )
+        return self.trace
+
+    def report(self):
+        report = defaultdict(lambda: defaultdict(int))
+        lc = len(self._calls)
+        for k, call in enumerate(self._calls):
+            if k == lc - 1:
+                # ignore last call
+                continue
+            next_call = self._calls[k + 1]
+            report[call["l"]]["nb"] += 1
+            report[call["l"]]["d"] += next_call["t"] - call["t"]
+
+        logs = ["calls  ms          code\n------------------------"]
+        for no, line in enumerate(self._lines):
+            data = report[no + self._firstline]
+            d = _colorize(data["d"]) if data else ""
+            logs.append("%-7s%-12s%s" % (data.get("nb", ""), d, line[:-1]))
+
+        logs.append("\nTotal time: %sms" % (_colorize(sum(d["d"] for d in report.values())),))
+        return "\n".join(logs)
+
+
+def trace(func):
+    """Decorate a function to trace its execution time."""
+
+    @wraps(func)
+    def decorator(*args, **kwargs):
+        tracer = _Tracer()
+        try:
+            sys.settrace(tracer.trace)
+            ret = func(*args, **kwargs)
+        finally:
+            sys.settrace(None)
+
+        _logger.log(NEARLYWARN, "tracing function %s:\n%s", func.__name__, tracer.report())
+
+        return ret
+
+    return decorator
