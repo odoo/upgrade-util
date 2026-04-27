0e5bd206f460681c7de3203597b77ff82b2181c0
Alvaro Fuentes <afu@odoo.com>
2026-04-27 14:28:16 +0200

    [IMP] util/logger: add `getLogger`
    Since Odoo 16 we now have a nice `__name__` for loaded upgrade scripts.
    For older versions we can still generate one. In this patch we provide a
    new function to get a "canonical" logger for an upgrade script.
    
    closes odoo/upgrade-util#425
    
    Related: odoo/upgrade#10057
    Signed-off-by: Christophe Simonis (chs) <chs@odoo.com>

--- src/util/logger.py
+++ src/util/logger.py
@@ -2,12 +2,40 @@
 import atexit
 import logging
 import os
+import sys
 
 from .misc import on_CI
 
 _REGISTERED = False
 
 _logger = logging.getLogger(__name__.rpartition(".")[0])
+
+
+def getLogger(name=None):
+    """
+    Return a logger named after the caller.
+
+    If `name` is a dotter module, it is used as-is. Otherwise the logger name is taken
+    from the caller's `__name__` when set to a dotted module name (Odoo 16+), or built
+    from the last three components of the caller's file path as
+    `odoo.upgrade.<module>.<version>.<script>`. `<script>` would be `name` parameter if
+    set and non-dotted.
+    """
+    if name and "." in name:
+        return logging.getLogger(name)
+    frame = sys._getframe(1)
+    script = ""
+    if name is None:
+        name = frame.f_globals.get("__name__") or ""
+    else:
+        script = name
+    if "." not in name:
+        path = frame.f_globals.get("__file__") or ""
+        parts = os.path.normpath(path).split(os.sep)
+        module, version, script = parts[-3], parts[-2], script or os.path.splitext(parts[-1])[0]
+        prefix = __name__.rsplit(".", 2)[0]
+        name = ".".join([prefix, module, version, script])
+    return logging.getLogger(name)
 
 
 class CriticalHandler(logging.Handler):
