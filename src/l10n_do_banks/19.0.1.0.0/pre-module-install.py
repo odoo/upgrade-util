883b8f90f6aab6fbc85747964a52190a6d5072d6
Erick Cuesto <erick@progressa.group>
2026-05-15 15:47:04 -0400

    feat(l10n_do_banks/19.0.1.0.0): force-install account_payment_internal_transfer when account_payment_cash_custom_workflow    is installed

---
+++ src/l10n_do_banks/19.0.1.0.0/pre-module-install.py
@@ -0,0 +1,25 @@
+"""
+Pre-migration script for l10n_do_banks 19.0.1.0.0
+
+Force-installs replacement modules whose functionality has been split out
+of legacy custom modules in v19.
+
+Installs performed:
+  - account_payment_internal_transfer (if account_payment_cash_custom_workflow is installed)
+"""
+
+import logging
+
+from odoo.addons.base.maintenance.migrations import util as mig_util
+
+_logger = logging.getLogger(__name__)
+
+_INSTALLS = [
+    ("account_payment_internal_transfer", ["account_payment_cash_custom_workflow"]),
+]
+
+
+def migrate(cr, version):
+    for module, if_installed in _INSTALLS:
+        mig_util.force_install_module(cr, module, if_installed=if_installed)
+        _logger.info("Module force-installed: %r (if installed: %s)", module, if_installed)
